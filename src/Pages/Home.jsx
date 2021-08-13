0dc0ab66ace322c4f216200c0b9429da7cdb186f
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-08-13 14:15:45 -0300

    home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../Style/Home.css'
 
 function Home() {
@@ -6,9 +7,9 @@ function Home() {
     <div className="body">
       <div className="subBody">
         <div>
-          <div className="intro title">
-            <i className='bx bxs-info-square bx-lg infoIcon' ></i>
-            <h1 className="title">I'm Julio Cesar</h1>
+          <div className="intro">
+            <i className='bx bxs-info-square bx-lg infoIcon'></i>
+            <h1 className="">I'm Julio Cesar</h1>
           </div>
           <div className="subTitle">
             <h4>Currently studying Software Development at Trybe</h4>
@@ -16,10 +17,12 @@ function Home() {
         </div>
         <img src="https://picsum.photos/536/354" alt="randow" className="randowImage" title="Don't like it? Try reloading the page ;)"/>
       </div>
-      <button type="button" className="startButton">
-        <i class='bx bxs-right-arrow bx-sm' ></i>
-        <p>Start</p>
-      </button>
+      <Link to="/explore" className="Link">
+        <button type="button" className="startButton">
+          <i className='bx bxs-right-arrow bx-sm' ></i>
+          <p>Get start</p>
+        </button>
+      </Link>
     </div>
   );
 }

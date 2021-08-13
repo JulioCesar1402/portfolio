4ad1bbebbeb5a3e5b7f71c5f3160ccf4a3074255
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-08-13 20:47:04 -0300

    about redirect to contact

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../Style/About.css';
 import Thumb from '../Images/thumb.png'
 import MyIcon from '../Images/photo.jpeg'
+import { Link } from 'react-router-dom';
 
 function About() {
   return (
@@ -17,14 +18,18 @@ function About() {
         <p>Currently studying Software Development at Trybe, a school that teaches how to program, learn and work, seeking to spread my passion for technology around the world, in order to always seek to learn more and more.</p>
         </div>
       </div>
-      <div className="knowledge">
-        <i className='bx bxl-javascript bx-md icon'></i>
-        <i className='bx bxl-html5 bx-md icon'></i>
-        <i className='bx bxl-css3 bx-md icon'></i>
-        <i className='bx bxl-react bx-md icon'></i>
-        <i className='bx bxl-redux bx-md icon'></i>
-        <i className='bx bxl-git bx-md icon'></i>
-        <button type="button" className="contact-about">Contact</button>
+      <div className="knowledge-contact">
+        <div className="knowledge">
+          <i className='bx bxl-javascript bx-md icon'></i>
+          <i className='bx bxl-html5 bx-md icon'></i>
+          <i className='bx bxl-css3 bx-md icon'></i>
+          <i className='bx bxl-react bx-md icon'></i>
+          <i className='bx bxl-redux bx-md icon'></i>
+          <i className='bx bxl-git bx-md icon'></i>
+        </div>
+        <Link to="/contact">
+          <button type="button" className="contact-about">Contact</button>
+        </Link>
       </div>
     </div>
   );

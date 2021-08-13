20cdff3309c00d7e3e942acb89d81b8684eb5bb5
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-08-13 20:01:56 -0300

    update about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,9 +5,27 @@ import MyIcon from '../Images/photo.jpeg'
 
 function About() {
   return (
-    <div className="bodyAbout">
-      <img src={Thumb} alt="randow" className="imageThumb" title="Don't like it? Try reloading the page ;)"/>
-      <img src={MyIcon} alt="randow" className="MyIcon" title="Don't like it? Try reloading the page ;)"/>
+    <div className="headerAbout">
+      <div className="bodyAbout">
+        <img src={Thumb} alt="randow" className="imageThumb"/>
+        <img src={MyIcon} alt="randow" className="MyIcon"/>
+      </div>
+      <div className="about">
+        <h3>Julio Cesar</h3>
+        <p className="subtitle-about">Web Developer</p>
+        <div className="summary">
+        <p>Currently studying Software Development at Trybe, a school that teaches how to program, learn and work, seeking to spread my passion for technology around the world, in order to always seek to learn more and more.</p>
+        </div>
+      </div>
+      <div className="knowledge">
+        <i className='bx bxl-javascript bx-md icon'></i>
+        <i className='bx bxl-html5 bx-md icon'></i>
+        <i className='bx bxl-css3 bx-md icon'></i>
+        <i className='bx bxl-react bx-md icon'></i>
+        <i className='bx bxl-redux bx-md icon'></i>
+        <i className='bx bxl-git bx-md icon'></i>
+        <button type="button" className="contact-about">Contact</button>
+      </div>
     </div>
   );
 }

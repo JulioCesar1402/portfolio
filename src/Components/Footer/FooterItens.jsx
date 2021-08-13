473ffe258175079be40d36138edee2aaeb06eeb1
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-08-13 09:04:25 -0300

    footer and navbar responsive

--- src/Components/Footer/FooterItens.jsx
+++ src/Components/Footer/FooterItens.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../Style/NavBar.css';
-import './Footer.css';
 
 function FooterItens() {
   return (
     <ul className="FooterItens">
       <li className="navItem" title="Home">
-        <Link to="/">
-          <i class='bx bx-home-alt bx-sm iconStyle'></i>
+        <Link to="/" className="navItemTag">
+          <i class='bx bx-home-alt bx-sm'></i>
+          <p className="textLink">Home</p>
         </Link>
       </li>
       <li className="navItem" title="Projects">
-        <Link to="/projects">
-        <i class='bx bx-folder bx-sm iconStyle'></i>
+        <Link to="/projects" className="navItemTag">
+          <i class='bx bx-folder bx-sm'></i>
+          <p className="textLink">Projects</p>
         </Link>
       </li>
       <li className="navItem" title="Games">
-        <Link to="/games">
-          <i class='bx bx-joystick-button bx-sm iconStyle'></i>
+        <Link to="/games" className="navItemTag">
+          <i class='bx bx-joystick-button bx-sm'></i>
+          <p className="textLink">Games</p>
         </Link>
       </li>
       <li className="navItem" title="Contact">
-        <Link to="/contact">
-          <i class='bx bx-paper-plane bx-sm iconStyle'></i>
+        <Link to="/contact" className="navItemTag">
+          <i class='bx bx-paper-plane bx-sm'></i>
+          <p className="textLink">Contact</p>
         </Link>
       </li>
       <li className="navItem" title="About">
-        <Link to="/about">
-          <i class='bx bx-user bx-sm iconStyle'></i>
+        <Link to="/about" className="navItemTag">
+          <i class='bx bx-user bx-sm'></i>
+          <p className="textLink">About</p>
         </Link>
       </li>
     </ul>

d70498a898a4d1d847b8e04f5c82f2e0f3841de4
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-08-14 20:54:31 -0300

    projects template

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import MainContext from '../Context/MainContext';
 import '../Style/Projects.css'
+import site from '../Images/siteTest.jpeg'
 
 function Projects({ location: { pathname } }) {
   const { setProjectsNavBar,
@@ -16,9 +17,61 @@ function Projects({ location: { pathname } }) {
     }
   }, [pathname, setProjectsNavBar, setHomeActive, setExploreActive, setAboutActive])
   return (
-    <div>
-      oi Sou a Projects
-    </div>
+    <section style={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
+      <div style={{display: "flex", width: "10%"}}>
+        <div class="card">
+          <img src={site} alt="Avatar" />
+          <div class="container">
+            <h4><b>Site 1</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+        <div class="card">
+          <img src={site} alt="Avatar" />
+          <div class="container">
+            <h4><b>Site 2</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+      </div>
+      <div style={{display: "flex", width: "10%"}}>
+        <div class="card">
+          <img src={site} alt="Avatar" />
+          <div class="container">
+            <h4><b>Site 1</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+        <div class="card">
+          <img src={site} alt="Avatar" />
+          <div class="container">
+            <h4><b>Site 2</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+      </div>
+      <div style={{display: "flex", width: "10%"}}>
+        <div class="card">
+          <img src={site} alt="Avatar" />
+          <div class="container">
+            <h4><b>Site 1</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+        <div class="card">
+          <img src={site} alt="Avatar" />
+          <div class="container">
+            <h4><b>Site 2</b></h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+      </div>
+      {/* <div style={{display: "flex", justifyContent: 'center', botton: "-500px"}}>
+        <button style={{backgroundColor: "#0096B7", color: "#0096B7", width: "20px", height: "20px", borderRadius: "50%"}}>0</button>
+        <button style={{backgroundColor: "#0096B7", color: "#0096B7", width: "20px", height: "20px", borderRadius: "50%"}}>0</button>
+        <button style={{backgroundColor: "#0096B7", color: "#0096B7", width: "20px", height: "20px", borderRadius: "50%"}}>0</button>
+      </div> */}
+    </section>
   );
 }
 
